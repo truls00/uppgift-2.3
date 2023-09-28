32d0df1e61a373b2c28f0b7593493ba2b3233a8b
Pingvu <nhlpool27@gmail.com>
2023-09-28 13:19:07 +0200

    Add project files.

---
+++ uppgift 2.3/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+namespace Uppgift_2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur många dagar ska du ha bilen?");
+            double X = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Och hur många kilometer ska du köra?");
+            double Z = double.Parse(Console.ReadLine());
+
+            double Y = X * 500 - 500;
+            double m = Z * 1;
+
+            double summa = m + Y + 300;
+
+            Console.WriteLine("Den totala kostnaden blir " + summa);
+            Console.ReadKey();
+
+        }
+    }
+}
+
